bb0753a39fd45305aeba5f60b9340f0361ce8873
Marat <Shagiakhmetov2499@gmail.com>
2021-06-02 09:28:40 +0300

    Initial Commit

---
+++ Traffic light/Traffic_lightApp.swift
@@ -0,0 +1,17 @@
+//
+//  Traffic_lightApp.swift
+//  Traffic light
+//
+//  Created by Marat Shagiakhmetov on 02.06.2021.
+//
+
+import SwiftUI
+
+@main
+struct Traffic_lightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
